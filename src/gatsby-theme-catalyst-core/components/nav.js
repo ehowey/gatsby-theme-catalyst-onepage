c72bbbdfc79a5346daa6485b474882f0c458c69e
Eric Howey <ehowey@gmail.com>
2019-07-09 23:02:24 -0600

    Beginning to update onepage

--- src/gatsby-theme-catalyst-core/components/nav.js
+++ src/gatsby-theme-catalyst-core/components/nav.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import { StaticQuery, graphql } from "gatsby"
 import styled from 'styled-components';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
