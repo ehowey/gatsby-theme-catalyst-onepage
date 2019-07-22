f75b89ec6adf153562bd2de350cad888311d1360
Eric Howey <ehowey@gmail.com>
2019-07-21 22:32:17 -0600

    Added comments

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -4,10 +4,10 @@ import { baseTheme } from "gatsby-theme-catalyst-core"
 export default merge({}, baseTheme, {
     //Updated theme options go here.
     sizes: {
-        mainMarginTop: "60px",
-        mainMarginTopTablet: "80px",
-        mainMarginTopLaptop: "80px"
-    },
+        mainMarginTop: "60px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+        mainMarginTopTablet: "80px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+        mainMarginTopLaptop: "80px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+      },
     catalystOptions: {
         headerPosition: "fixed",
     },
