968fc56cccdfa58a56324e9473c1a5e482d9563b
Eric Howey <ehowey@gmail.com>
2019-07-27 06:32:34 -0600

    Formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,14 @@ module.exports = {
     description: `Extends the base catalyst core theme to create a one page Gatsby theme`,
     author: `Eric Howey`
   },
-    plugins: [
-      'gatsby-theme-catalyst-core',
-      {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            name: 'src',
-            path: `${__dirname}/src/`
-        }
+  plugins: [
+    `gatsby-theme-catalyst-core`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
       }
-    ]
-}
+    }
+  ]
+};
