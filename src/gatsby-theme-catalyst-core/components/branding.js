16ce699cf3edb268164c4140824f5d141310ae7e
Eric Howey <ehowey@gmail.com>
2019-07-26 19:54:40 -0600

    Formatting

--- src/gatsby-theme-catalyst-core/components/branding.js
+++ src/gatsby-theme-catalyst-core/components/branding.js
@@ -1,62 +1,70 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
-import Img from "gatsby-image"
-import { useStaticQuery, graphql } from "gatsby"
+import { jsx } from "theme-ui";
+import Img from "gatsby-image";
+import { useStaticQuery, graphql } from "gatsby";
 import { animateScroll as scroll } from "react-scroll";
 
 const scrollToTop = () => {
   scroll.scrollToTop();
-}
+};
 
 const siteBranding = () => {
   const data = useStaticQuery(graphql`
-        query {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-          brandingLogo: file(relativePath: { eq: "images/logo.png" }) {
-            childImageSharp {
-              fluid(maxHeight: 300) {
-                ...GatsbyImageSharpFluid_withWebp
-              }
-            }
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      brandingLogo: file(relativePath: { eq: "images/logo.png" }) {
+        childImageSharp {
+          fluid(maxHeight: 300) {
+            ...GatsbyImageSharpFluid_withWebp
           }
         }
-      `)
-      return(
-        <div
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            ml: 2,
-            mr: 2,
-            cursor: "pointer"
-          }}
-          onClick={scrollToTop}
-        >
-            <Img
-              sx={{
-                // Uses width because of weird bug with flex box and shrinking content we don't want shrunk
-                width: [theme => theme.sizes.logoWidth, theme => theme.sizes.logoWidthTablet, theme => theme.sizes.logoWidthLaptop],
-                mr: 1,
-                filter: [props.open ? theme => theme.catalystOptions.invertLogo : "none", "none", null]
-              }}
-              fluid={data.brandingLogo.childImageSharp.fluid}
-              alt={data.site.siteMetadata.title}
-              imgStyle={{ objectFit: 'contain' }}
-            />
-          <h1
-            sx={{
-              fontSize: [4, 5, 6],
-              flex: "0 0 auto"
-            }}
-          >
-              {data.site.siteMetadata.title}
-          </h1>
-        </div>
-  )
-            }
+      }
+    }
+  `);
+  return (
+    <div
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        ml: 2,
+        mr: 2,
+        cursor: "pointer"
+      }}
+      onClick={scrollToTop}
+    >
+      <Img
+        sx={{
+          // Uses width because of weird bug with flex box and shrinking content we don't want shrunk
+          width: [
+            theme => theme.sizes.logoWidth,
+            theme => theme.sizes.logoWidthTablet,
+            theme => theme.sizes.logoWidthLaptop
+          ],
+          mr: 1,
+          filter: [
+            props.open ? theme => theme.catalystOptions.invertLogo : "none",
+            "none",
+            null
+          ]
+        }}
+        fluid={data.brandingLogo.childImageSharp.fluid}
+        alt={data.site.siteMetadata.title}
+        imgStyle={{ objectFit: "contain" }}
+      />
+      <h1
+        sx={{
+          fontSize: [4, 5, 6],
+          flex: "0 0 auto"
+        }}
+      >
+        {data.site.siteMetadata.title}
+      </h1>
+    </div>
+  );
+};
 
-export default siteBranding
+export default siteBranding;
--- src/gatsby-theme-catalyst-core/components/nav.js
+++ src/gatsby-theme-catalyst-core/components/nav.js
@@ -1,102 +1,106 @@
 /** @jsx jsx */
-import { jsx, useThemeUI } from "theme-ui"
-import { useStaticQuery, graphql } from "gatsby"
-import { Link } from "react-scroll"
+import { jsx, useThemeUI } from "theme-ui";
+import { useStaticQuery, graphql } from "gatsby";
+import { Link } from "react-scroll";
 
 const siteNav = props => {
-  if (typeof window !== 'undefined') {
-    let w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth
-    var screenWidth = x
+  if (typeof window !== "undefined") {
+    let w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementsByTagName("body")[0],
+      x = w.innerWidth || e.clientWidth || g.clientWidth;
+    var screenWidth = x;
   }
-  const {theme} = useThemeUI()
+  const { theme } = useThemeUI();
 
-  let navoffset = parseInt(theme.sizes.headerHeight)
+  let navoffset = parseInt(theme.sizes.headerHeight);
 
   if (screenWidth >= parseInt(theme.breakpoints[0])) {
-    navoffset = parseInt(theme.sizes.headerHeightTablet)
+    navoffset = parseInt(theme.sizes.headerHeightTablet);
   } else if (screenWidth >= parseInt(theme.breakpoints[1])) {
-    navoffset = parseInt(theme.sizes.headerHeightLaptop)
+    navoffset = parseInt(theme.sizes.headerHeightLaptop);
   }
 
   const data = useStaticQuery(graphql`
-      query {
-        site {
-          siteMetadata {
-            anchorLinks {
-              name
-              link
-            }
+    query {
+      site {
+        siteMetadata {
+          anchorLinks {
+            name
+            link
           }
         }
       }
-      `)
-      const {theme} = useThemeUI()
-      const headOpen = () => {
-        return (
-          window.innerHeight - parseInt(theme.sizes.headerHeight) + "px"
-        )
-      }
-      return(
-        <nav
-          sx={{
-            gridColumn: ["1 / -1", "2 / 3", null],
-            gridRow: ["2 / 3", "1 / 2", null],
-            justifySelf: ["center", "end", null],
-            alignSelf: "center",
-            height: [props.open ? headOpen() : 0, "auto", null],
-            mt: [props.open ? 2 : 0, 0, null],
-          }}
-          role="navigation"
-          aria-label="main-navigation"
-        >
-          <ul
-            sx={{
-              display: [props.open ? "flex" : "none", "flex", null],
-              flexDirection: ["column", "row", null],
-              listStyle: "none",
-              margin: 0,
+    }
+  `);
+  const { theme } = useThemeUI();
+  const headOpen = () => {
+    return window.innerHeight - parseInt(theme.sizes.headerHeight) + "px";
+  };
+  return (
+    <nav
+      sx={{
+        gridColumn: ["1 / -1", "2 / 3", null],
+        gridRow: ["2 / 3", "1 / 2", null],
+        justifySelf: ["center", "end", null],
+        alignSelf: "center",
+        height: [props.open ? headOpen() : 0, "auto", null],
+        mt: [props.open ? 2 : 0, 0, null]
+      }}
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <ul
+        sx={{
+          display: [props.open ? "flex" : "none", "flex", null],
+          flexDirection: ["column", "row", null],
+          listStyle: "none",
+          margin: 0,
 
-              ".active": {
-                fontWeight: "bold"
-              }
-            }}
-            aria-label="menu-bar"
-            role="menubar"
-            >
-            {data.site.siteMetadata.anchorLinks.map(link => (
-            <li
+          ".active": {
+            fontWeight: "bold"
+          }
+        }}
+        aria-label="menu-bar"
+        role="menubar"
+      >
+        {data.site.siteMetadata.anchorLinks.map(link => (
+          <li
             sx={{
               my: 0,
               mx: 1
             }}
             key={link.name}
             role="none"
+          >
+            <Link
+              sx={{
+                color: props.open ? "header.textOpen" : "header.text",
+                textDecoration: "none",
+                py: 2,
+                px: 1,
+                mr: 2,
+                cursor: "pointer"
+              }}
+              to={link.link.replace(/#/g, "").toLowerCase()}
+              onClick={e => {
+                props.action(e);
+              }}
+              role="menuitem"
+              spy={true}
+              smooth={true}
+              activeClass="active"
+              duration={500}
+              offset={-Math.abs(navoffset)}
             >
-              <Link
-                sx={{
-                  color: props.open ? "header.textOpen" : "header.text",
-                  textDecoration: "none",
-                  py: 2,
-                  px: 1,
-                  mr: 2,
-                  cursor: "pointer"
-                }}
-                to={link.link.replace(/#/g,"").toLowerCase()}
-                onClick={(e) => {props.action(e)}}
-                role="menuitem"
-                spy={true}
-                smooth={true}
-                activeClass="active"
-                duration={500}
-                offset={-Math.abs(navoffset)}
-              >
-                {link.name}
-              </Link>
-            </li>
-            ))}
-          </ul>
-      </nav>
-    )
-  }
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default siteNav;
