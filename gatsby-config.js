1d027c3459995b2416378ebdfc0875057609cb5b
Eric Howey <ehowey@gmail.com>
2019-07-30 22:54:10 -0600

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Catalyst Onepage`,
     description: `Extends the base catalyst core theme to create a one page Gatsby theme`,
-    author: `Eric Howey`
+    author: `Eric Howey`,
+    siteUrl: `https://www.example.com`
   },
-  plugins: [
-    `gatsby-theme-catalyst-core`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`
-      }
-    }
-  ]
+  plugins: [`gatsby-theme-catalyst-core`]
 };
