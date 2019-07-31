fd166a95abe36d71dbede14ca83574c4fd427908
Eric Howey <ehowey@gmail.com>
2019-07-30 23:54:25 -0600

    Further componentize the navbar area

--- src/gatsby-theme-catalyst-core/components/nav.js
+++ src/gatsby-theme-catalyst-core/components/nav.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx, useThemeUI } from "theme-ui";
 import NavLinks from "./navLinks";
-import { SocialHeader } from "gatsby-theme-catalyst-core";
+import { SocialHeaderIcons, SocialLinks } from "gatsby-theme-catalyst-core";
 
 const siteNav = props => {
   const { theme } = useThemeUI();
@@ -26,29 +26,9 @@ const siteNav = props => {
       aria-label="main-navigation"
     >
       <NavLinks {...props} />
-      <div
-        sx={{
-          display: [props.open ? "flex" : "none", "flex", null],
-          mr: ["auto", 3, null],
-          ml: "auto",
-          mt: [3, 0, null],
-
-          a: {
-            color: props.open ? "header.iconsOpen" : "header.icons",
-            mr: [3, 2, null],
-            height: theme => theme.sizes.iconsHeader,
-            textDecoration: "none"
-          },
-          "a:last-of-type": {
-            mr: 0
-          },
-          "a:hover": {
-            color: "header.iconsHover"
-          }
-        }}
-      >
-        <SocialHeader />
-      </div>
+      <SocialLinks>
+        <SocialHeaderIcons />
+      </SocialLinks>
     </nav>
   );
 };
